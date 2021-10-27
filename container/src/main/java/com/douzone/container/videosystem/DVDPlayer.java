d6ec95a8e78e8767a7a76ab4f583a24769f59dbb
pocpoc0202 <pocpoc0202@naver.com>
2021-10-27 16:21:42 +0900

    Explicit Config

--- container/src/main/java/com/douzone/container/videosystem/DVDPlayer.java
+++ container/src/main/java/com/douzone/container/videosystem/DVDPlayer.java
@@ -3,19 +3,18 @@ package com.douzone.container.videosystem;
 public class DVDPlayer {
 	private DigitalVideoDisc dvd;
 	
-	// 기본 생성자가 없으면 에러남
-	public DVD
-	
+	public DVDPlayer() {
+	}
 	
 	public DVDPlayer(DigitalVideoDisc dvd) {
 		this.dvd = dvd;
 	}
 	
-	public void play() {
-		dvd.play();
-	}
-
 	public void setDvd(DigitalVideoDisc dvd) {
 		this.dvd = dvd;
+	}
+
+	public void play() {
+		dvd.play();
 	}
 }
